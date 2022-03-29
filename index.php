6aa9eb9cf2593d800eb0c23b531a60201ec003a4
M1571 <94639639+M1571@users.noreply.github.com>
2022-03-29 16:47:16 +0200

    fine con correzione

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $text_lenght= strlen($text);
 
 // -----------------------------
 
-$bad_word= $_GET['$bad_word']; 
+$bad_word= $_GET['bad_word']; 
 $cens_bad_word= str_replace($bad_word, $cens, $text);
 // lunghezza censura
 $cens_lenght= strlen($cens_bad_word);
@@ -28,6 +28,7 @@ $cens_lenght= strlen($cens_bad_word);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
